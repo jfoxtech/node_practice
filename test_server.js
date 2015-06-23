d198956d566cc6af4199582a29f92004d3822d6e
Justin Fox <jfox@grassroots-tech.com>
2015-06-23 12:09:25 -0400

    Add all other exercises and instructions to the files.

--- test_server.js
+++ test_server.js
@@ -6,7 +6,10 @@ var http = require('http');
 
 http.createServer(function(request, response){
 	response.writeHead(200);
-	response.write("Hello this is dog.");
-	response.end();
+	response.write("Hello this is dog.\n");
+	setTimeout(function(){
+		response.write("Dog is done.");
+		response.end();
+	}, 5000);
 }).listen(8081);
 console.log("...Listening on port 8081");
